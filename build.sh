33acc40fc9715770f37eed7a288a20ab85aa8d0c
minz1 <minz1@github.com>
2017-03-07 18:04:59 -0500

    build.sh: toolchain update

--- build.sh
+++ build.sh
@@ -14,9 +14,9 @@ KERNEL_DIR=$PWD
 DTBTOOL=$KERNEL_DIR/dtbTool
 cd $KERNEL_DIR
 export ARCH=arm64
-export CROSS_COMPILE="/home/dev/Desktop/aarch64-a72.a53-linux-gnueabi/bin/aarch64-a72.a53-linux-gnueabi-"
-export LD_LIBRARY_PATH=home/dev/Desktop/aarch64-a72.a53-linux-gnueabi/lib/
-STRIP="/home/dev/Desktop/Toolchain/aarch64-a72.a53-linux-gnueabi/bin/aarch64-a72.a53-linux-gnueabi-strip"
+export CROSS_COMPILE="/home/dev/Desktop/aarch64-cortex_a72.a53-linux-gnueabi/bin/aarch64-cortex_a72.a53-linux-gnueabi-"
+export LD_LIBRARY_PATH=home/dev/Desktop/aarch64-cortex_a72.a53-linux-gnueabi/lib/
+STRIP="/home/dev/Desktop/Toolchain/aarch64-cortex_a72.a53-linux-gnueabi/bin/aarch64-cortex_a72.a53-linux-gnueabi-strip"
 make clean
 make masterKernel_defconfig
 export KBUILD_BUILD_HOST="Ubuntu-Xenial"
